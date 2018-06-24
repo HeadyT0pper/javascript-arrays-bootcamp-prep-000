8a7ec5275975bea127bd9187459122d843c03f31
Jonathan Stricklin <jstricklin89@gmail.com>
2018-06-24 20:41:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,4 +16,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return 3
 }
-function 
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.unshift(element)
+  return array
+}
