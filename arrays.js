650261b7a5b4c822fb02c775fc0ffcfba8c9527c
Jonathan Stricklin <jstricklin89@gmail.com>
2018-06-24 20:52:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,6 @@ function accessElementInArray(array, index) {
   return 3
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(element)
+  array.shift()
   return array
 }
