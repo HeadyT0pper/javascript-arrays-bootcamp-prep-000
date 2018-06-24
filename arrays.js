f2cd890227a83a2f1a3745d864cf6c14e9c20c19
Jonathan Stricklin <jstricklin89@gmail.com>
2018-06-24 20:05:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,4 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var = addElementToBeginningOfArray
 var = destructivelyAddElementToBeginningOfArray
 var = addElementToEndOfArray
-var = 
+var = destructivelyAddElementToEndOfArray
